13249ea4713e299a9fcc4c86afee8189d507472e
reuterShawn <reutershawn@proton.me>
2024-08-26 00:19:06 -0500

    Add coontact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,15 @@
+<svelte:head>
+	<title>Contact</title>
+	<meta name="description" content="About this app" />
+</svelte:head>
+
+<script>
+	import Contact from "$lib/components/Contact.svelte";
+</script>
+
+<div class="text-column">
+	<p>
+		Welcome to my website where you can learn all about cooking from home delicious meals from scratch.
+	</p>
+    <Contact/>
+</div>
