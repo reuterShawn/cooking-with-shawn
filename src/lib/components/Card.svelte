1e48a58e8ce0836ec5891fbcd4a2c9644c91b14e
reuterShawn <reutershawn@proton.me>
2024-08-26 00:18:03 -0500

    Style Card

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -7,9 +7,10 @@
 <div class="card-container">
   {#each cards as card}
     <div class="card">
-      <img src={card.imageUrl} alt={card.title} />
+      <img src={card.imageUrl} alt={card.title}/>
       <h2>{card.title}</h2>
       <p>{card.description}</p>
+      <button>ADD ITEM</button>
     </div>
   {/each}
 </div>
@@ -22,13 +23,25 @@
   }
 
   .card {
-    border: 1px solid #ccc;
+    border: .5em solid orange;
+    border-radius: 1em;
     padding: 1rem;
-    width: 200px;
+    box-shadow:inset 2px;
+  }
+
+  button {
+    background-color: orange;
+    width: 6em;
+    float: right;
+    border-radius: 1em;
+    box-shadow: inset;
+    padding: .5em;
   }
 
   img {
     max-width: 100%;
+  
     height: auto;
   }
+  
 </style>
