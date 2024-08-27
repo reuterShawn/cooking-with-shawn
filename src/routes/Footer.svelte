3f8edbec53694dac551ab2ee2de15ea1dd93a2f4
reuterShawn <reutershawn@proton.me>
2024-08-27 15:23:19 -0500

    Add footer

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,44 @@
+<script>
+</script>
+
+<footer>
+	<nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+	</nav>
+
+</footer>
+
+<style>
+
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 2em;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+  }
+
+
+  li {
+    margin-left: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+</style>
