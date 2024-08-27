8c5f57038830adc3fcba6a7839d05ffcbb4f1f3b
reuterShawn <reutershawn@proton.me>
2024-08-27 00:42:36 -0500

    Update styles

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -7,7 +7,7 @@
 <div class="card-container">
   {#each cards as card}
     <div class="card">
-      <img src={card.imageUrl} alt={card.title}/>
+      <img src={card.imageUrl} alt={card.title} style="width: 10em; height: 100px;"/>
       <h2>{card.title}</h2>
       <p>{card.description}</p>
       <button>ADD ITEM</button>
@@ -18,15 +18,19 @@
 <style>
   .card-container {
     display: flex;
+  
     flex-wrap: wrap;
     gap: 1rem;
   }
 
   .card {
+    width: 14em;
+    height: 20em;
     border: .5em solid orange;
     border-radius: 1em;
-    padding: 1rem;
+    padding-left: .5em;
     box-shadow:inset 2px;
+    justify-content: flex-start;
   }
 
   button {
@@ -36,11 +40,12 @@
     border-radius: 1em;
     box-shadow: inset;
     padding: .5em;
+    color: white;
   }
 
   img {
     max-width: 100%;
-  
+    object-fit:fill;
     height: auto;
   }
   
