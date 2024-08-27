0b66b7b78eb303199e113792069c80888f542822
reuterShawn <reutershawn@proton.me>
2024-08-26 23:13:21 -0500

    Comment out server

--- src/routes/api/sendgrid/+server.js
+++ src/routes/api/sendgrid/+server.js
@@ -1,24 +1,23 @@
-import { SENDGRID_API_KEY } from '../$.env';
-import sgMail from '@sendgrid/mail';
-import { json } from '@sveltejs/kit';
+// import { SENDGRID_API_KEY } from '../$.env';
+// import sgMail from '@sendgrid/mail';
+// import { json } from '@sveltejs/kit';
 
-sgMail.setApiKey(SENDGRID_API_KEY);
+// sgMail.setApiKey(SENDGRID_API_KEY);
 
-export async function POST({ request }) {
-  const { to, subject, text } = await request.json();
+// export async function POST({ request }) {
+//   const { to, subject, text } = await request.json();
 
-  const msg = {
-    to,
-    from: 'reutershawn45@gmail.com', // Use the email address you verified with SendGrid
-    subject,
-    text,
-  };
+//   const msg = {
+//     to,
+//     from: 'reutershawn45@gmail.com', // Use the email address you verified with SendGrid
+//     subject,
+//     text,
+//   };
 
-  try {
-    await sgMail.send(msg);
-    return json({ success: true });
-  } catch (e) {
-    console.error(e);
-    return json({ success: false, error: e.message }, { status: 500 });
-  }
-}
+//   try {
+//     await sgMail.send(msg);
+//     return json({ success: true });
+//   } catch (e) {
+//     console.error(e);
+//     return json({ success: false, error: e.message }, { status: 500 });
+//   }
